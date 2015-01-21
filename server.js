f4766ea3ee86b04dfb75a6e07510535fb76dbdf1
schrikvis <schrikvis@gmail.com>
2015-01-21 20:07:01 +0100

    reimplementing sort

--- server.js
+++ server.js
@@ -98,14 +98,14 @@ var App = function(){
 	var reqLevelName = req.param('LevelName');
 	//var collection = self.db.get('AllTimes');
 	//self.db.collection('AllTimes').find(
-	self.db.collection('AllTimes').find(
-	{
-	'LevelName' : reqLevelName
-	}
-	).toArray(function(err, names){
+	var cursor = self.db.collection('AllTimes').find({'LevelName' : reqLevelName});
+	cursor.limit(0);
+	cursor.sort({'Time' : -1});
+	cursor.toArray(function(err, names){
               res.header("Content-Type:","application/json");
               res.end(JSON.stringify(names));
-      });
+			}
+	  );
   });
 	/*,
 	function(err, cursor){
